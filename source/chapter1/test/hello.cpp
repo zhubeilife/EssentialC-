cc30ae2657280e1fd61586da078f95489dc70bb2
zhubeilife <zhubeilife@gmail.com>
2022-07-17 18:25:03 +0800

    add chapter 1 test

--- source/chapter1/test/hello.cpp
+++ source/chapter1/test/hello.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <fstream>
+#include <iomanip>
 
 void test_input() {
   std::string user_name;
@@ -31,8 +33,46 @@ void test_pointer() {
   int *pi = &ival;
 }
 
-int main() {
-  test_pointer();
+void test_read_and_write() {
+  std::ofstream outfile("seq_data.txt", std::ios_base::app);
+  if ( !outfile ) {
+    std::cerr << "something wrong with it";
+  }
+  else {
+    outfile << "append file to it"
+            << std::endl;
+  }
+}
 
+void test_1_5_cstyle() {
+  int num_size = 128;
+  char name[num_size];
+  std::cout << "Please Input the name";
+  std::cin >> std::setw(num_size) >> name;
+  std::cout << "Get:" << name;
+}
+
+void test_1_7() {
+  std::ifstream in_file("input.txt");
+  if (!in_file) {
+    std::cerr << "Failed to open the file";
+    return;
+  }
+
+  std::string word;
+  while (in_file >> word) {
+    std::cout << word << std::endl;
+  }
+}
+
+void test_1_8() {
+  char* show_message[] = {
+      "hello",
+      "world"
+  }
+}
+
+int main() {
+  test_1_7();
   return 0;
 }
